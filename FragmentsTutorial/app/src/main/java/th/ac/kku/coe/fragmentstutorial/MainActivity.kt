00942cc5ccbe24b61fca48cfa803c557d86e52a0
chakriyaP <65884346+chakriyaP@users.noreply.github.com>
2021-02-10 15:25:01 +0700

    jaaa

---
+++ FragmentsTutorial/app/src/main/java/th/ac/kku/coe/fragmentstutorial/MainActivity.kt
@@ -0,0 +1,17 @@
+package th.ac.kku.coe.fragmentstutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, MainFragment())
+                .commit()
+        }
+    }
+}
